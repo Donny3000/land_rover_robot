b90a1b5ef9ff4f2bbf2d079c527b1f9a690d61cb
Donald R. Poole, Jr <dpoole30@gatech.edu>
2019-04-19 06:03:58 +0000

    Adding encoder message

--- auto_rover_speed_controller/arduino/AutoRoverSpeedController/src/main.cpp
+++ auto_rover_speed_controller/arduino/AutoRoverSpeedController/src/main.cpp
@@ -3,10 +3,18 @@
 #define USE_USBCON
 #include <ros.h>
 #include <geometry_msgs/Twist.h>
+#include <RedBot.h>
 
-#define SPEED_MAX 400
+#define SPEED_MAX    400
+#define CNTS_PER_REV 100
 
-DualVNH5019MotorShield md;
+// Configure library with pins as remapped for single-channel operation
+// this lets the single motor be controlled as if it were "motor 1"
+DualVNH5019MotorShield md(2, 7, 9, 6, A0, 2, 7, 9, 12, A1);
+RedBotEncoder encoders(A3, A4);
+uint32_t front_cnt;
+uint32_t rear_cnt;
+
 ros::NodeHandle nh;
 
 geometry_msgs::Twist resp;
@@ -16,7 +24,7 @@ ros::Publisher cmd_vel_resp("auto_rover/cmd_vel_resp", &resp);
 void joystickCb(const geometry_msgs::Twist& vel)
 {
     // The the linear portion for the motor speed
-  md.setM1Speed(vel.linear.x);
+  md.setM1Speed(static_cast<int16_t>(vel.linear.x));
 
   resp.linear.x = vel.linear.x;
   resp.angular.z = vel.angular.z;
@@ -27,6 +35,9 @@ void setup()
 {
   // Setup the LED for diagnostics
   pinMode(LED_BUILTIN, OUTPUT);
+
+  // Clear both motor encoders
+  encoders.clearEnc(BOTH);
 
   // Setup Motor Controller
   md.init();

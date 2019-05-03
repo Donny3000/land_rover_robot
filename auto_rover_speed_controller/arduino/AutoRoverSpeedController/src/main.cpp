57b899570296b24f8c237c21d5e79e123f0108e3
Donald R. Poole, Jr <donny3000 (at) gmail (dot) com>
2019-05-03 07:54:23 -0500

    Removing IMU code and moving to a Raspberry Pi

--- auto_rover_speed_controller/arduino/AutoRoverSpeedController/src/main.cpp
+++ auto_rover_speed_controller/arduino/AutoRoverSpeedController/src/main.cpp
@@ -1,12 +1,10 @@
 #include <Servo.h>
 #include <Wire.h>
 #include <DualVNH5019MotorShield.h>
-#include "DFRobot_BNO055.h"
 
 //#define USE_USBCON
 #include <ros.h>
 #include <geometry_msgs/Twist.h>
-#include <sensor_msgs/Imu.h>
 #include <auto_rover_speed_controller/Encoders.h>
 
 #define SPEED_MAX         400
@@ -16,14 +14,9 @@
 #define REAR_WHEEL_SERVO  11
 #define SERVO_OFFSET      90
 
-typedef DFRobot_BNO055_IIC BNO; // ******** use abbreviations instead of full names ********
-
 // Configure library with pins as remapped for single-channel operation
 // this lets the single motor be controlled as if it were "motor 1"
 DualVNH5019MotorShield md(2, 7, 9, 6, A0, 2, 7, 9, 12, A1);
-
-// Setup the DFRobot IMU
-BNO bno_(&Wire, 0x28);
 
 // Setup the servo for front_wheels_
 Servo front_wheels_;
@@ -37,13 +30,7 @@ volatile bool up_;
 auto_rover_speed_controller::Encoders encoder_;
 ros::Publisher encoder_pub_("auto_rover/encoders", &encoder_);
 
-// Setup IMU publisher
-sensor_msgs::Imu imu_;
-BNO::sAxisAnalog_t sGyrAnalog, sLiaAnalog, sGrvAnalog;
-BNO::sQuaAnalog_t  sQuaAnalog;
-ros::Publisher imu_pub_("auto_rover/imu/data", &imu_);
-
-void isr()
+void encoder_isr()
 {
   if (digitalRead(ENCODER_CH_A))
     up_ = digitalRead(ENCODER_CH_B);
@@ -81,16 +68,6 @@ void setup()
   // Setup the LED for diagnostics
   pinMode(LED_BUILTIN, OUTPUT);
 
-  bno_.reset();
-  while (bno_.begin() != BNO::eStatusOK)
-  {
-    digitalWrite(LED_BUILTIN, HIGH);
-    delay(100);
-    digitalWrite(LED_BUILTIN, LOW);
-    delay(100);
-  }
-//   mpu.setMode(mpu.eNORMAL_POWER_MODE, mpu.eFASTEST_MODE);
-
   // Setup the servo
   front_wheels_.attach(FRONT_WHEEL_SERVO);
   rear_wheels_.attach(REAR_WHEEL_SERVO);
@@ -98,7 +75,7 @@ void setup()
   // Clear both motor encoders
   pinMode(ENCODER_CH_A, INPUT_PULLUP);
   pinMode(ENCODER_CH_B, INPUT_PULLUP);
-  attachInterrupt(digitalPinToInterrupt(ENCODER_CH_A), isr, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(ENCODER_CH_A), encoder_isr, CHANGE);
 
   // Setup Motor Controller
   md.init();

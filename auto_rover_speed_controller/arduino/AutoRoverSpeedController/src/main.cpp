1a863839ecc3b61c7a8e5c274ad029f975113659
Donald R. Poole, Jr <donny3000 (at) gmail (dot) com>
2019-05-13 00:11:38 -0500

    Adding code for new encoder setup

--- auto_rover_speed_controller/arduino/AutoRoverSpeedController/src/main.cpp
+++ auto_rover_speed_controller/arduino/AutoRoverSpeedController/src/main.cpp
@@ -7,12 +7,14 @@
 #include <geometry_msgs/Twist.h>
 #include <auto_rover_speed_controller/Encoders.h>
 
-#define SPEED_MAX         400
-#define ENCODER_CH_A      19
-#define ENCODER_CH_B      18
-#define FRONT_WHEEL_SERVO 3
-#define REAR_WHEEL_SERVO  11
-#define SERVO_OFFSET      90
+#define SPEED_MAX          400
+#define REAR_LEFT_ENCODER  19
+#define REAR_RIGHT_ENCODER 18
+#define FRONT_WHEEL_SERVO  3
+#define REAR_WHEEL_SERVO   11
+#define SERVO_OFFSET       90
+
+unsigned long start_time_;
 
 // Configure library with pins as remapped for single-channel operation
 // this lets the single motor be controlled as if it were "motor 1"
@@ -25,27 +27,28 @@ Servo rear_wheels_;
 ros::NodeHandle nh;
 
 // Wheel Encoder Setup
-volatile bool fired_;
-volatile bool up_;
+volatile uint16_t rear_left_ticks_, rear_right_ticks_;
+volatile uint16_t front_left_ticks_, front_right_ticks_;
 auto_rover_speed_controller::Encoders encoder_;
 ros::Publisher encoder_pub_("auto_rover/encoders", &encoder_);
 
-void encoder_isr()
+void isr_rear_left_encoder()
 {
-  if (digitalRead(ENCODER_CH_A))
-    up_ = digitalRead(ENCODER_CH_B);
-  else
-    up_ = !digitalRead(ENCODER_CH_B);
-
-  fired_ = true;
+  rear_left_ticks_++;
 }
 
-int16_t prev_speed, cur_speed, prev_steer, cur_steer;
+void isr_rear_right_encoder()
+{
+  rear_right_ticks_++;
+}
+
 // ROS Subscriber to read joystick command
 void joystickCb(const geometry_msgs::Twist& vel)
 {
-  cur_speed = static_cast<int16_t>(vel.linear.x);
-  cur_steer = static_cast<int16_t>(vel.angular.z) + SERVO_OFFSET;
+  static int16_t prev_speed = 0, prev_steer = 0;
+
+  int16_t cur_speed = static_cast<int16_t>(vel.linear.x);
+  int16_t cur_steer = static_cast<int16_t>(vel.angular.z) + SERVO_OFFSET;
 
   // The the linear portion for the motor speed
   if (cur_speed != prev_speed)
@@ -68,14 +71,21 @@ void setup()
   // Setup the LED for diagnostics
   pinMode(LED_BUILTIN, OUTPUT);
 
+  // Initialize the encoder ticks
+  rear_left_ticks_ = rear_right_ticks_ = 0;
+  front_left_ticks_ = front_right_ticks_ = 0;
+  encoder_.rear_left_ = encoder_.rear_right_ = 0;
+  encoder_.front_left_ = encoder_.front_right_ = 0;
+
   // Setup the servo
   front_wheels_.attach(FRONT_WHEEL_SERVO);
   rear_wheels_.attach(REAR_WHEEL_SERVO);
 
   // Clear both motor encoders
-  pinMode(ENCODER_CH_A, INPUT_PULLUP);
-  pinMode(ENCODER_CH_B, INPUT_PULLUP);
-  attachInterrupt(digitalPinToInterrupt(ENCODER_CH_A), encoder_isr, CHANGE);
+  pinMode(REAR_LEFT_ENCODER, INPUT_PULLUP);
+  pinMode(REAR_RIGHT_ENCODER, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(REAR_LEFT_ENCODER), isr_rear_left_encoder, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(REAR_RIGHT_ENCODER), isr_rear_right_encoder, CHANGE);
 
   // Setup Motor Controller
   md.init();
@@ -88,7 +98,7 @@ void setup()
 
 void loop()
 {
-  nh.spinOnce();
+  start_time_ = millis();
   delay(10);
 
   if (!nh.connected())
@@ -98,15 +108,14 @@ void loop()
     digitalWrite(LED_BUILTIN, LOW);
     delay(500);
   }
-  else if (fired_)
+  else
   {
-    if (up_)
-      encoder_.wheel_ticks++;
-    else
-      encoder_.wheel_ticks--;
+    encoder_.millis_ = millis() - start_time_;
+    encoder_.rear_left_ = rear_left_ticks_;
+    encoder_.rear_right_ = rear_right_ticks_;
 
     encoder_pub_.publish( &encoder_ );
-
-    fired_ = false;
   }
+
+  nh.spinOnce();
 }

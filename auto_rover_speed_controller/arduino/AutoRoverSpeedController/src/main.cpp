3c9a5a642fd696f5c50fecdb357dcefdd1c51bff
Donald R. Poole, Jr <donny3000 (at) gmail (dot) com>
2019-05-16 00:11:10 -0500

    Updating arduino code and gitignore

--- auto_rover_speed_controller/arduino/AutoRoverSpeedController/src/main.cpp
+++ auto_rover_speed_controller/arduino/AutoRoverSpeedController/src/main.cpp
@@ -29,42 +29,20 @@ ros::NodeHandle nh;
 // Wheel Encoder Setup
 volatile uint16_t rear_left_ticks_, rear_right_ticks_;
 volatile uint16_t front_left_ticks_, front_right_ticks_;
-auto_rover_speed_controller::Encoders encoder_;
-ros::Publisher encoder_pub_("auto_rover/encoders", &encoder_);
 
-void isr_rear_left_encoder()
-{
-  rear_left_ticks_++;
-}
+// ISRs
+void isr_rear_left_encoder();
+void isr_rear_right_encoder();
 
-void isr_rear_right_encoder()
-{
-  rear_right_ticks_++;
-}
+void attachEncoders();
+void detachEncoders();
 
 // ROS Subscriber to read joystick command
-void joystickCb(const geometry_msgs::Twist& vel)
-{
-  static int16_t prev_speed = 0, prev_steer = 0;
-
-  int16_t cur_speed = static_cast<int16_t>(vel.linear.x);
-  int16_t cur_steer = static_cast<int16_t>(vel.angular.z) + SERVO_OFFSET;
-
-  // The the linear portion for the motor speed
-  if (cur_speed != prev_speed)
-  {
-    md.setM1Speed(cur_speed);
-    prev_speed = cur_speed;
-  }
-
-  if (cur_steer != prev_steer)
-  {
-    front_wheels_.write(cur_steer);
-    rear_wheels_.write(-cur_steer);
-    prev_steer = cur_steer;
-  }
-}
+void joystickCb(const geometry_msgs::Twist& vel);
 ros::Subscriber<geometry_msgs::Twist> sub("auto_rover/cmd_vel", &joystickCb);
+
+auto_rover_speed_controller::Encoders encoder_;
+ros::Publisher encoder_pub_("auto_rover/encoders", &encoder_);
 
 void setup()
 {
@@ -74,8 +52,6 @@ void setup()
   // Initialize the encoder ticks
   rear_left_ticks_ = rear_right_ticks_ = 0;
   front_left_ticks_ = front_right_ticks_ = 0;
-  encoder_.rear_left_ = encoder_.rear_right_ = 0;
-  encoder_.front_left_ = encoder_.front_right_ = 0;
 
   // Setup the servo
   front_wheels_.attach(FRONT_WHEEL_SERVO);
@@ -84,8 +60,7 @@ void setup()
   // Clear both motor encoders
   pinMode(REAR_LEFT_ENCODER, INPUT_PULLUP);
   pinMode(REAR_RIGHT_ENCODER, INPUT_PULLUP);
-  attachInterrupt(digitalPinToInterrupt(REAR_LEFT_ENCODER), isr_rear_left_encoder, CHANGE);
-  attachInterrupt(digitalPinToInterrupt(REAR_RIGHT_ENCODER), isr_rear_right_encoder, CHANGE);
+  attachEncoders();
 
   // Setup Motor Controller
   md.init();
@@ -110,12 +85,62 @@ void loop()
   }
   else
   {
+    // Safeguard calculations and publishing of the message
+    detachEncoders();
+
     encoder_.millis_ = millis() - start_time_;
     encoder_.rear_left_ = rear_left_ticks_;
     encoder_.rear_right_ = rear_right_ticks_;
+    rear_left_ticks_ = rear_right_ticks_ = 0;
 
     encoder_pub_.publish( &encoder_ );
+    attachEncoders();
   }
 
   nh.spinOnce();
+}
+
+void isr_rear_left_encoder()
+{
+  rear_left_ticks_++;
+}
+
+void isr_rear_right_encoder()
+{
+  rear_right_ticks_++;
+}
+
+void attachEncoders()
+{
+  attachInterrupt(digitalPinToInterrupt(REAR_LEFT_ENCODER), isr_rear_left_encoder, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(REAR_RIGHT_ENCODER), isr_rear_right_encoder, CHANGE);
+}
+
+void detachEncoders()
+{
+  detachInterrupt(digitalPinToInterrupt(REAR_LEFT_ENCODER));
+  detachInterrupt(digitalPinToInterrupt(REAR_RIGHT_ENCODER));
+}
+
+// ROS Subscriber to read joystick command
+void joystickCb(const geometry_msgs::Twist& vel)
+{
+  static int16_t prev_speed = 0, prev_steer = 0;
+
+  int16_t cur_speed = static_cast<int16_t>(vel.linear.x);
+  int16_t cur_steer = static_cast<int16_t>(vel.angular.z) + SERVO_OFFSET;
+
+  // The the linear portion for the motor speed
+  if (cur_speed != prev_speed)
+  {
+    md.setM1Speed(cur_speed);
+    prev_speed = cur_speed;
+  }
+
+  if (cur_steer != prev_steer)
+  {
+    front_wheels_.write(cur_steer);
+    rear_wheels_.write(-cur_steer);
+    prev_steer = cur_steer;
+  }
 }
